b7c418dd7796e92152f41302ce37b0246b85097a
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-11 15:44:29 +0000

    ISS Spotter with Promises complete.

--- index2.js
+++ index2.js
@@ -1,8 +1,52 @@
-const { nextIssTimesForMyLocation } = require('./iss_promised');
+//don't forget to destructure {  } the module.exports object
+const { nextISSFlightTimesForMyLocation } = require('./iss_promised');
+// ------Mentor Question ---------- what??
 const { printPassTimes } = require('./index');
 
 
-nextIssTimesForMyLocation()
-  // .catch((error) => {
-  //   console.log("it didn't work", error.message);
-  // });
+//invoke the main function
+nextISSFlightTimesForMyLocation()
+  //logging the "response" array to the console.
+  //since it's passed in as an argument to the "body" parameter for this .then
+  // .then(body => console.log(body));
+  //--------------------------------------
+  //but that's not a very userfriendly output... milliseconds since Jan 1 1970 is not how I think about time as a human. Let's do some conversion.
+  //to do the conversion, require index from the previous callback version of this 
+  //this is a function printPassTimes which takes an argument
+  //Take a look at this syntax. The callback we are using here involves the data which we are calling "body", and the printPassTimes function. So we need to pass in both the data and an anon callback which invokes that printPassTimes function with the data.
+  /************** MENTOR QUESTION *****************/
+  //const printPassTimes automatically invokes that function without invoking it from within the below .this method.
+  //i have no idea why it does that. Perhaps contact a mentor about this.
+  // .this((body) => {
+  //   printPassTimes(body);
+  // })
+  //--------------------------------------
+  //so now we put on our catch. what we want to log is the message value from the error object
+  .catch((e) => {
+    console.log("It didn't work:", e.message);
+  })
+
+/*
+---------historic code left here for notes purposes--------
+
+call the first function
+fetchMyIP()
+-----------------------------
+we are then calling the next function and passing in the value returned from fetchMyIP
+this .then is calling fetchCoordsByIP as a callback to fetchMyIP
+that's why it's written like a callback, and doesn't explicity take a callback itself (to avoid callback hell).
+remember nesting cb(data)? This is that same format. When we call fetchCoordsByIP, it already knows about the (body) Promise, because chaining is like calling it from inside fetchMyIP which returned it.
+Don't forget - a lot of the syntax is working under the hood of request-promise-native and Promise object & chaining methods build that you don't know about yet, so that's why you might not intuitively get it immediately
+.then(fetchCoordsByIP)
+-----------------------------
+calling fetchISSFlyOverTimes as a callback to fetchCoordsByIP
+.then(fetchISSFlyOverTimes)
+-----------------------------
+this .then takes a callback that simply prints the returned value to the console
+.then(body => console.log(body))
+-----------------------------
+.catch takes a callback with a parameter. I don't know why it's in double brackets but the .then above is not.
+.catch((e) => {
+  console.log("It didn't work:", e.message);
+});
+*/
--- iss_promised.js
+++ iss_promised.js
@@ -1,30 +1,57 @@
 const request = require('request-promise-native');
-const ipApiUrl = 'https://api.ipify.org?format=json';
-const coordApiUrl = 'https://ipvigilante.com/';
 
-const fetchMyIp = () => {
-  //all we need is request, because it's got a promise thing now
-  return request(ipApiUrl);
+const fetchMyIP = () => {
+  // request(url, (e, r, b)) <-- in OG request, we needed the callback as the second argument. in request-promise-native, it returns a promise, so we don't need the callback (because it returns...)
+  // Don't forget to return!
+  return request('https://api.ipify.org?format=json');
 };
 
-const fetchCoordsByIp = (body) => {
-  const coordUrl = coordApiUrl + JSON.parse(body).ip;
-  return request(coordUrl);
-}
+const fetchCoordsByIP = (body) => {
+  //via promises and chaining, we are passing in the 'body' returned from the fetchMyIP function above. It's in JSON format, so we need to parse out just the value string
+  //we parse (body).ip even though it's only one K:V pair because we just want the V
+  ip = JSON.parse(body).ip;
+  //switching this to a template literal with the url in it cuz declaring it up top is actually less readable IMO
+  //return!
+  return request(`https://ipvigilante.com/${ip}`);
+};
 
-const fetchIssFlyOverTimes = (coords) => {
-  const { latitude, longitude } = JSON.parse(coords).data;
+const fetchISSFlyOverTimes = (body) => {
+  //passing in the data returned from fetchCoordsByIP (parameter still referred to as "body")
+  //need to JSON.parse out just the lat/long. We can do that in the cool destructuring way that I need to learn
+  //so in setting up the { latitude, longitude } on the left-hand side, the right-hand side of the function knows that within "data" we are just looking for the values associated with the "latitude" and "longitude" keys. 
+  //for clarity, the '.data' piece is because the coords JSON has two KV pairs in it, and the second one's key is called "data" and its value is the data object which contains the lat and long properties
+  const { latitude, longitude } = JSON.parse(body).data
+  //request to ISS API using a template literal because then it's easier to interpolate in the lat long obtained with the destructuring syntax
   return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
-}
+  //don't forget to return ;) 
+};
 
-const nextIssTimesForMyLocation = () => {
-  fetchMyIp()
-    .then(fetchCoordsByIp)
-    .then(fetchIssFlyOverTimes)
+//we are now going to refactor to package all the functions above into one, using the chaining protocol, so that we only have to call one function from our index doc before we do some other magical things on that doc to display the output
+//since we are invoking this function from the beginning, it won't take any arguments itself
+const nextISSFlightTimesForMyLocation = () => {
+  //it will call fetchMyIP, which it knows about cuz it's in the global scope
+  //note that when we call fetchMyIP, it sets off the chain of callbacks below, which will ultimately return "response"
+  //we need to make this higher order function return something too. To do that, we place return at the very beginning of the chain. 
+  //this of it like this:
+  //higherOrderFunction = () => {
+  //return valueComputedByCallbacks. 
+  //but we can't declare this valueComputedByCallbacks by triggering the chain and then writing return below, because of async. so we need to write the return inline with invoking the first function in the chain
+  //}
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    //but now we have some JSON data to parse.
+    //hen there's an anonymouse callback inside a .then callback, you need to pass in the data from the previous return, and that callback, so the anon callback knows what to operate on. 
+    //this differs from the then(body => console.log) because console.log is not a function, it's an expression that prints to console. instead, anon callback is a function that will return a value
     .then((data) => {
+      //we are interested in an array of objects that is the value of the "response" key
+      //we will use destructuring syntax for this expression, because we want to store an array of objects as well (same format), but we are lazy and only want to write one line of code instead of two or three.
+      //deciphering this desctructing syntax: we will store the value of each object {  } element within the array associated with the .response key, nested together within an array. We will refer to this array as the variable const reponse
       const { response } = JSON.parse(data);
       return response;
     });
-}
+};
 
-module.exports = { nextIssTimesForMyLocation };
+module.exports = {
+  nextISSFlightTimesForMyLocation
+};
